508be9617f9083a7b729445c8e37cfda6ff739df
Kyle <kyleratti@users.noreply.github.com>
2020-11-21 16:24:20 -0500

    feat(status): add support for GET /status
    This also moves the 'no clients' and 'too many clients' error messages into middleware

--- src/socketserver.ts
+++ src/socketserver.ts
@@ -11,15 +11,41 @@ export default class SocketServer {
   private server = http.createServer(this.app);
   private io = socketIo(this.server);
 
+  private get clients(): { [id: string]: socketIo.Socket } {
+    return this.io.sockets.sockets;
+  }
+
+  private get numClients(): Number {
+    return Object.keys(this.clients).length;
+  }
+
+  private noClientsMiddleware = (_req, res, next: Function) => {
+    if (!this.clients || this.numClients <= 0)
+      return res.status(HttpStatusCodes.SERVICE_UNAVAILABLE).json({
+        error: `StatCrew client unavailable (is the Stat Crew VM connected to the relay?)`,
+      });
+
+    next();
+  };
+
+  private tooManyClientsMiddleware = (_req, res, next: Function) => {
+    if (this.numClients > 1)
+      return res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).json({
+        error: `StatCrew clients > 1 (too many StatCrew listeners)`,
+      });
+
+    next();
+  };
+
   start = () => {
     this.server.listen(port, () => {
       console.log(`Listening for HTTP requests on http://127.0.0.1:${port}`);
     });
 
-    this.io.on("connect", socket => {
+    this.io.on("connect", (socket) => {
       console.log(`Client connected`);
 
-      socket.on("message", msg => {
+      socket.on("message", (msg) => {
         console.debug(`Received message`, msg);
       });
 
@@ -28,43 +54,48 @@ export default class SocketServer {
       });
     });
 
-    this.app.get("/:fileName.xml", async (req, res) => {
-      const fileName = req.params.fileName;
-      const fullFileName = `${fileName}.xml`;
-      const clients = this.io.sockets.sockets;
-      const numClients = Object.keys(clients).length;
-
-      if (!clients || numClients <= 0)
-        return res.status(HttpStatusCodes.SERVICE_UNAVAILABLE).json({
-          error: `StatCrew client unavailable (is the Stat Crew VM connected to the relay?)`
-        });
-
-      if (numClients > 1)
-        return res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).json({
-          error: `StatCrew clients > 1 (too many StatCrew listeners)`
-        });
-
-      const client = clients[Object.keys(clients)[0]];
-
-      const relayReq = new RelayRequest({
-        socket: client,
-        fileName: fullFileName
-      });
-
-      try {
-        const payload = await relayReq.send();
-
-        return res
-          .status(HttpStatusCodes.OK)
-          .contentType(".xml")
-          .send(payload.data);
-      } catch (e) {
-        console.error(e);
-
-        return res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).json({
-          error: e
+    this.app.get(
+      "/status",
+      this.noClientsMiddleware,
+      this.tooManyClientsMiddleware,
+      (_req, res) => {
+        return res.status(HttpStatusCodes.OK).json({
+          message: "OK",
+          client: this.clients[0],
         });
       }
-    });
+    );
+
+    this.app.get(
+      "/:fileName.xml",
+      this.noClientsMiddleware,
+      this.tooManyClientsMiddleware,
+      async (req, res) => {
+        const fileName = req.params.fileName;
+        const fullFileName = `${fileName}.xml`;
+
+        const client = this.clients[Object.keys(this.clients)[0]];
+
+        const relayReq = new RelayRequest({
+          socket: client,
+          fileName: fullFileName,
+        });
+
+        try {
+          const payload = await relayReq.send();
+
+          return res
+            .status(HttpStatusCodes.OK)
+            .contentType(".xml")
+            .send(payload.data);
+        } catch (e) {
+          console.error(e);
+
+          return res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).json({
+            error: e,
+          });
+        }
+      }
+    );
   };
 }
